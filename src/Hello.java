97fe5970e86a92e5a328947f5c100c0cec97290c
fanwenzhong <fanwenzhong@foton.com.cn>
2021-11-09 16:06:36 +0800

    测试第三次提交文件

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     String name="fanwenzhong";
     int id;
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        System.out.println(123);
+    }
 }
